1fe9710e7031d725078ad8cc1080dc53124b5c45
jusnani <jusnaniusman@gnail.com>
2025-09-27 17:34:38 +0800

    pertemuan2

--- pertemuan2.py
+++ pertemuan2.py
@@ -5,3 +5,13 @@ elif nilai > 90:
     print("aku pintar banget")
 else:
     print("aku tidak punya nilai")
+
+angka = 0
+while True:
+    print("aku belajar pyhton",angka)
+    if angka == 5:
+        break
+    angka += 1
+
+for i in range(10):
+    print("aku sedang berulang",i)
